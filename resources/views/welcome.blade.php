83aaac17fdbdbe5b270a2cecf28ffb0f8eb08af8
Marcos <devmarcos7@gmail.com>
2025-03-14 14:53:30 -0300

    ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>CRUD - Tarefas</title>
+        <title>Estudos sobre laravel</title>
     </head>
 
     <body>
